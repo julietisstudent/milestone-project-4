c0684edb688316506c0a5d3e2e1c4c6dcd3416af
Juliet <jewlz19@hotmail.co.uk>
2019-06-12 19:30:32 +0000

    modified settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -87,12 +87,12 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
 
-DATABASES = {
-     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-   }
-}
+#DATABASES = {
+#     'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#   }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
